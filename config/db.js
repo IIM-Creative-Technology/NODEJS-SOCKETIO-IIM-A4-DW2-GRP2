5394d4739fd583e9b85e2e81e4d10a4a89515e0d
AntoineDrsl <antoine.durussel@gmail.com>
2022-04-26 18:24:43 +0200

    fix(db.js): update db connexion

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 //const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5431/nodejspg');
 const sequelize = new Sequelize('nodejspg', 'postgres', 'postgres', {
-    host: 'localhost',
-    port: '5431',
+    host: 'nodejs-pg',
+    port: '5432',
     dialect:'postgres',
 });
 
