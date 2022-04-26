2bf71e9215c801ff046b39facbe2114126cf70a3
Julien <julien.quennehen@edu.devinci.fr>
2022-04-26 10:02:46 +0200

    refactor: 💡 add a route POST in routes
    add a route POST in the file routes/route.js, this POST route is at the
    root of the server

--- routes/route.js
+++ routes/route.js
@@ -5,3 +5,8 @@ var app = express();
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
+
+//Répond 'Got a POST request' avec une méthode POST sur la route racine (/)
+app.post("/", function (req, res) {
+  res.send("Got a POST request");
+});
