bb5a85fb8e04a6279651d896072276ce1b2a3c65
AntoineDrsl <antoine.durussel@gmail.com>
2022-04-26 10:08:34 +0200

    feat(index.js): add socket to server

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express');
-const app = express();
 const port = 3000;
 
-app.listen(port, () => {
+const express = require('express');
+const { createServer } = require("http");
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+});
+
+// Socket IO
+io.on('connection', socket => {
+
+  socket.on('to-server', () => {
+    console.log('Socket received !');
+  });
+
 });
