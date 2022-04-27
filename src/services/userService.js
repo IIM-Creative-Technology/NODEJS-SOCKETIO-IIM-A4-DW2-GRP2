fe9bbf814a17ef7f3026d859f3f8d951136d5434
Julien Dupont <julien.dupont2018@gmail.com>
2022-04-27 21:46:45 +0200

    feat(users): add search users
    supports pagination and search

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,25 +1,50 @@
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require("sequelize");
 const sequelize = require("../models").sequelize;
 const bcrypt = require("bcryptjs");
+const {Op} = require("sequelize");
 
 const User = require(`../models/user`)(sequelize, DataTypes);
 
 class UserService {
-  async createUser(userData) {
-    const newUser = User.build({ ...userData });
+    async createUser(userData) {
+        const newUser = User.build({...userData});
 
-    newUser.password = bcrypt.hashSync(newUser.password, 12);
+        newUser.password = bcrypt.hashSync(newUser.password, 12);
 
-    return await newUser.save();
-  }
+        return await newUser.save();
+    }
 
-  async deleteUser(id) {
-    return await User.destroy({
-      where: {
-        id: id
-      },
-    })
-  }
+    async searchUsers({offset = 0, limit = 20, search = ''}) {
+        return await User.findAndCountAll({
+            attributes: {
+                exclude: ['password']
+            },
+            where:
+                {
+                    [Op.or]: {
+                        email: {
+                            [Op.substring]: search
+                        },
+                        lastName: {
+                            [Op.substring]: search
+                        },
+                        firstName: {
+                            [Op.substring]: search
+                        }
+                    }
+                },
+            offset,
+            limit
+        })
+    }
+
+    async deleteUser(id) {
+        return await User.destroy({
+            where: {
+                id: id
+            },
+        })
+    }
 }
 
 module.exports = UserService;
