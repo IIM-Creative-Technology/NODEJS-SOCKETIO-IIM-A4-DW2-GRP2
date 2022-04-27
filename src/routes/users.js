ca91acf425470d16f9c78774eee0cc7126bf84f3
Mathis Angibaud <mathisangibaud@macbook-pro-de-mathis.home>
2022-04-27 16:29:04 +0200

    refactor(user): route findAll call (NEED TO FIX IT)

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../../models/user");
+const { user } = require("../models");
 
 router.post("/", (req, res) => {
   // #swagger.summary = 'Create a new user';
@@ -10,20 +10,24 @@ router.post("/", (req, res) => {
 router.get("/:id", async (req, res) => {
   // #swagger.summary = 'Get user by id'
   // #swagger.tags = ['Users']
-  const user = await User.findAll({
-    where: {
-      id: req.params.id
-    }
-  });
-  console.log("User:", JSON.stringify(user));
+  // const user = await User.findAll({
+  //   where: {
+  //     id: req.params.id
+  //   }
+  // });
+  // console.log("User:", JSON.stringify(user));
   res.send(req.params);
 });
 
 router.get("/", async (req, res) => {
   // #swagger.summary = 'Get all users'
   // #swagger.tags = ['Users']
-  const users = await User.findAll();
+
+  //METHODE 1 BY ME
+  const users = await user.findAll();
   console.log("All users:", JSON.stringify(users));
+
+  // console.log(await user.findAll());
   res.send("get users");
 });
 
