3527ecf69eb40d712615423548ec47772904fbcd
MathiasGilles <fb132fce5>
2022-04-28 00:04:54 +0200

    create search documents by attributes method

--- src/services/documentService.js
+++ src/services/documentService.js
@@ -1,4 +1,5 @@
 const {DataTypes} = require("sequelize");
+const {Op} = require("sequelize/types");
 const sequelize = require("../models").sequelize;
 
 const Document = require(`../models/document`)(sequelize, DataTypes);
@@ -8,6 +9,27 @@ class DocumentService{
     async createDocument(documentData) {
         const newDocument = Document.build({...documentData});
         return await newDocument.save();
+    }
+
+    async searchDocuments({offset = 0, limit = 20, search = ''}) {
+        return await Document.findAndCountAll({
+            where:
+                {
+                    [Op.or]: {
+                        name: {
+                            [Op.substring]: search
+                        },
+                        uri: {
+                            [Op.substring]: search
+                        },
+                        creatorId: {
+                            [Op.substring]: search
+                        }
+                    }
+                },
+            offset,
+            limit
+        })
     }
 }
 
