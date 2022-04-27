efd4d57184fbb0eeb568578cef4c1426730f2b33
MathiasGilles <fb132fce5>
2022-04-28 00:33:34 +0200

    add new column in user model

--- src/migrations/20220427222933-modify_users_add_isadmin.js
+++ src/migrations/20220427222933-modify_users_add_isadmin.js
@@ -2,20 +2,21 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return Promise.all([
+      queryInterface.addColumn(
+          'users',
+          'isAdmin',
+          {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
+          },
+      ),
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return Promise.all([
+      queryInterface.removeColumn('users', 'isAdmin')
+    ]);
   }
 };
