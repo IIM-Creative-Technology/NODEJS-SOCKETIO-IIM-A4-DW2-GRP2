7cbd1609ba6148ebaac082265b3560a8b59a3f06
MathiasGilles <fb132fce5>
2022-04-27 23:58:25 +0200

    add route post to create document

--- src/routes/documents.js
+++ src/routes/documents.js
@@ -1,2 +1,16 @@
 const DocumentService = require("../services/documentService");
 const documentService = new DocumentService();
+
+const router = require("express").Router();
+
+router.post("/", (req, res) => {
+    // #swagger.summary = 'Create a new document';
+    // #swagger.tags = ['Documents']
+    try {
+        const result = documentService.createDocument(req.body);
+        res.send(result);
+    } catch (error) {
+        res.status(400);
+        res.send("An error occured while creating document");
+    }
+});
