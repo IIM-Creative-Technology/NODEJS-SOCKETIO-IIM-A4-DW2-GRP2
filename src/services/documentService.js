8017c19eeed74b323cdb03a3642081bfac358a2a
MathiasGilles <fb132fce5>
2022-04-27 23:58:06 +0200

    add create method in document service

--- src/services/documentService.js
+++ src/services/documentService.js
@@ -1,5 +1,14 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require("../models").sequelize;
+
+const Document = require(`../models/document`)(sequelize, DataTypes);
+
 class DocumentService{
 
+    async createDocument(documentData) {
+        const newDocument = Document.build({...documentData});
+        return await newDocument.save();
+    }
 }
 
 module.exports = DocumentService;
