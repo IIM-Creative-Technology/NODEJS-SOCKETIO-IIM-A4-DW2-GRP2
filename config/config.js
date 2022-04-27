f6ad03526809dae5edcd7a29f1060fec5e4a91af
MathiasGilles <fb132fce5>
2022-04-27 10:57:52 +0200

    add try catch to connect to bdd,and server close if not

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
-const {Sequelize} = require('sequelize');
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: 'localhost',
+    host: "nodejs-pg",
     port: process.env.DB_PORT,
     dialect: 'postgres'
 });
+
+module.exports = {sequelize};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const router = require('../routes/router');
 
+const db = require('../config/config');
+
 const express = require('express');
-const { createServer } = require("http");
-const { Server } = require('socket.io');
+const {createServer} = require("http");
+const {Server} = require('socket.io');
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-httpServer.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+httpServer.listen(port, async () => {
+    console.log(`Example app listening on port ${port}`)
+    try {
+        await db.sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        httpServer.close();
+    }
 });
 app.use('/', router);
 
 // Socket IO
 io.on('connection', socket => {
 
-  socket.on('to-server', () => {
-    console.log('Socket received from client !');
-    socket.emit('to-client');
-  });
+    socket.on('to-server', () => {
+        console.log('Socket received from client !');
+        socket.emit('to-client');
+    });
 
 });
