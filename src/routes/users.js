4258885474582759743bacb164cecd60ccb8ffe9
MathiasGilles <fb132fce5>
2022-04-28 09:10:53 +0200

    edit patch route by checking if user is admin

--- src/routes/users.js
+++ src/routes/users.js
@@ -93,7 +93,7 @@ router.patch("/:id", authJwt, async (req, res) => {
         const user = await userService.findUserById(req.params.id);
 
         if(user){
-            if(user.id !== req.user.id){
+            if(user.id !== req.user.id || !user.isAdmin){
                 res.status(403).send({message:'Not authorized'})
                 return
             }
